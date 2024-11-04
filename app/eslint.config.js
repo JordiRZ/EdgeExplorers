e8bd0dd8e8d33561c92c830fa3533bb47f15f31d
gemmas95 <gemma1030@gmail.com>
2024-11-04 19:07:29 +0100

    add lint to pre-push and add more prettier config

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -10,4 +10,15 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-uses-react': 'off',
+    },
+    settings: {
+    react: {
+      "version": "detect"
+    }
+  }
+  },
 ];
